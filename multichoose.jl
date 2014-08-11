5027bee10b249ea706442d36fe61ba5926f75bda
Stephanie Unna <steph.unna@gmail.com>
2014-08-11 15:58:10 +0100

    Tidying up multichoose function

--- multichoose.jl
+++ multichoose.jl
@@ -1,17 +1,21 @@
 # Generates a table with all combinations of elements from each choice.
-# The 'choices' option should be a tuple of length ncols, containing arrays of
-# values that each column can take.
+# The 'choices' option should be a tuple of arrays containing values that
+# each column can take.
 
-###############################################################################
-###############################################################################
+function multichoose(choices::Tuple{Array{Int64}})
+  ncols::Int64 = length(choices)
+  nrows::Int64 = prod([length(choices[i]) for i in 1:ncols])
+  multimat::Array{Int64} = zeros(Int64, nrows, ncols)
+  fill_column(1:nrows, ncols)
+end
 
 function fill_column(rows, cols)
   partitionsize::Int64 = (length(rows))/(length(choices[cols]))
   for j = 1:length(choices[cols])
-    c = cols
+    c::Int64 = cols
     partitionstart::Int64 = rows[1] + partitionsize * (j - 1)
     partitionend::Int64 = rows[1] - 1 + partitionsize * j
-    partitionrange = partitionstart:partitionend
+    partitionrange::UnitRange{Int64} = partitionstart:partitionend
     multimat[partitionrange, cols] = choices[c][j]
     c -= 1
     if c > 0
@@ -19,10 +23,3 @@ function fill_column(rows, cols)
     end
   end
 end
-
-choices = ([2, 40, 74], [5, 7], [10, 11, 5, 4])
-ncols = length(choices)
-nrows = prod([length(choices[i]) for i in 1:ncols])
-multimat = zeros(Int64, nrows, ncols)
-
-fill_column(1:nrows, ncols)
