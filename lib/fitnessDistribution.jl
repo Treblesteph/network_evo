e5047c145272868a6b877536885364411c5cc93d
Stephanie Unna <steph.unna@gmail.com>
2014-10-20 16:35:08 +0100

    Moved into lib.

---
+++ lib/fitnessDistribution.jl
@@ -0,0 +1,18 @@
+# Checking the distribution of fitness across a large number of randomly
+# generated networks.
+using Gadfly
+using Cairo
+
+require("evolveclock.jl")
+require("generate_networks.jl")
+using clockga.fitness
+
+NNETS = 100
+
+println("making population...")
+population = pmap(create_network, [1:NNETS])
+println(size(population))
+println("evaluating population...")
+scores = pmap(fitness, population)
+
+plot(x = scores, Geom.histogram)
