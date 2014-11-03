9e658c466270aee967800bd5c0f858758c631707
Stephanie Unna <steph.unna@gmail.com>
2014-11-03 15:13:11 +0000

    Changing number of runs.

--- lib/runboolclock.jl
+++ lib/runboolclock.jl
@@ -36,7 +36,7 @@ end
 tic()
 params = set_parameters()
 add_clock_params!(params, [repression, activation, noInteraction])
-model = runga(params, EvolveClock; init_pop_size = 100, stop_after = 10000)
+model = runga(params, EvolveClock; init_pop_size = 100, stop_after = 500)
 println()
 toc()
 now = strftime("%F_%H_%M", time())
