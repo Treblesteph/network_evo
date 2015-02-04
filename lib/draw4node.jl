79b4c25dba71344bac7022abdc3f05cfa2599233
Stephanie Unna <steph.unna@gmail.com>
2015-02-04 12:41:52 +0000

    Tidying up and condensing.

--- lib/draw4node.jl
+++ lib/draw4node.jl
@@ -2,17 +2,22 @@
 
 function draw4node(reps, acts, envs, gates)
 
+  canvas = 1        # Side length of square canvas
+  c1 = 3*canvas/10  # Lower centre co-ordinate
+  c2 = canvas-c1    # Upper centre co-ordinate
+  rad = canvas/10   # Node radius
+
   paths = acts + reps
 
   function drawall()
 
     compose(context(),
-           (context(), drawreps(reps)),
-           (context(), drawacts(acts)),
-           (context(), drawpaths(acts + reps)),
-           (context(), drawenvs(envs)),
-           (context(), drawgates(gates)),
-           (context(), drawgeneric()))
+           (context(), drawreps(reps, c1, c2, canvas, rad)),
+           (context(), drawacts(acts, c1, c2, canvas, rad)),
+           (context(), drawpaths(acts + reps, c1, c2, canvas, rad)),
+           (context(), drawenvs(envs, c1, c2, canvas, rad)),
+           (context(), drawgates(gates, c1, c2, canvas, rad)),
+           (context(), drawgeneric(canvas, c1, c2, rad, rad)))
   end
 
   img = PNG("../runs/netpic.png", 8inch, 8inch)
@@ -20,169 +25,62 @@ function draw4node(reps, acts, envs, gates)
 
 end
 
-function drawreps(reps)
+function drawreps(reps, c1, c2, canvas, rad)
 
-drawnreps = Array(Context, 16)
+drawnreps = Array(Any, 16)
 
 repcolour = stroke(LCHab(90, 0, 297))
 
-drawnreps[1] = compose(context(),
-                       line([(0.19, 0.28), (0.19, 0.32)]),
-                       linewidth(2mm),
-                       repcolour) # 1 to 1
+drawnreps[1] = ([(0.19, 0.28), (0.19, 0.32)]) # 1 to 1
+drawnreps[2] = ([(0.42, 0.25), (0.42, 0.29)]) # 2 to 1
+drawnreps[3] = ([(0.415, 0.405), (0.445, 0.375)]) # 3 to 1
+drawnreps[4] = ([(0.31, 0.42), (0.35, 0.42)]) # 4 to 1
+drawnreps[5] = ([(0.58, 0.31), (0.58, 0.35)]) # 1 to 2
+drawnreps[6] = ([(0.81, 0.28), (0.81, 0.32)]) # 2 to 2
+drawnreps[7] = ([(0.71, 0.42), (0.75, 0.42)]) # 3 to 2
+drawnreps[8] = ([(0.595, 0.415), (0.625, 0.445)]) # 4 to 2
+drawnreps[9] = ([(0.555, 0.625), (0.585, 0.595)]) # 1 to 3
+drawnreps[10] = ([(0.65, 0.58), (0.69, 0.58)]) # 2 to 3
+drawnreps[11] = ([(0.81, 0.68), (0.81, 0.72)]) # 3 to 3
+drawnreps[12] = ([(0.58, 0.71), (0.58, 0.75)]) # 4 to 3
+drawnreps[13] = ([(0.25, 0.58), (0.29, 0.58)]) # 1 to 4
+drawnreps[14] = ([(0.375, 0.555), (0.405, 0.585)]) # 2 to 4
+drawnreps[15] = ([(0.42, 0.65), (0.42, 0.69)]) # 3 to 4
+drawnreps[16] = ([(0.19, 0.68), (0.19, 0.72)]) # 4 to 4
 
-drawnreps[2] = compose(context(),
-                       line([(0.42, 0.25), (0.42, 0.29)]),
-                       linewidth(2mm),
-                       repcolour) # 2 to 1
-
-drawnreps[3] = compose(context(),
-                       line([(0.415, 0.405), (0.445, 0.375)]),
-                       linewidth(2mm),
-                       repcolour) # 3 to 1
-
-drawnreps[4] = compose(context(),
-                       line([(0.31, 0.42), (0.35, 0.42)]),
-                       linewidth(2mm),
-                       repcolour) # 4 to 1
-
-drawnreps[5] = compose(context(),
-                       line([(0.58, 0.31), (0.58, 0.35)]),
-                       linewidth(2mm),
-                       repcolour) # 1 to 2
-
-drawnreps[6] = compose(context(),
-                       line([(0.81, 0.28), (0.81, 0.32)]),
-                       linewidth(2mm),
-                       repcolour) # 2 to 2
-
-drawnreps[7] = compose(context(),
-                       line([(0.71, 0.42), (0.75, 0.42)]),
-                       linewidth(2mm),
-                       repcolour) # 3 to 2
-
-drawnreps[8] = compose(context(),
-                       line([(0.595, 0.415), (0.625, 0.445)]),
-                       linewidth(2mm),
-                       repcolour) # 4 to 2
-
-drawnreps[9] = compose(context(),
-                       line([(0.555, 0.625), (0.585, 0.595)]),
-                       linewidth(2mm),
-                       repcolour) # 1 to 3
-
-drawnreps[10] = compose(context(),
-                        line([(0.65, 0.58), (0.69, 0.58)]),
-                        linewidth(2mm),
-                        repcolour) # 2 to 3
-
-drawnreps[11] = compose(context(),
-                        line([(0.81, 0.68), (0.81, 0.72)]),
-                        linewidth(2mm),
-                        repcolour) # 3 to 3
-
-drawnreps[12] = compose(context(),
-                        line([(0.58, 0.71), (0.58, 0.75)]),
-                        linewidth(2mm),
-                        repcolour) # 4 to 3
-
-drawnreps[13] = compose(context(),
-                        line([(0.25, 0.58), (0.29, 0.58)]),
-                        linewidth(2mm),
-                        repcolour) # 1 to 4
-
-drawnreps[14] = compose(context(),
-                        line([(0.375, 0.555), (0.405, 0.585)]),
-                        linewidth(2mm),
-                        repcolour) # 2 to 4
-
-drawnreps[15] = compose(context(),
-                        line([(0.42, 0.65), (0.42, 0.69)]),
-                        linewidth(2mm),
-                        repcolour) # 3 to 4
-
-drawnreps[16] = compose(context(),
-                        line([(0.19, 0.68), (0.19, 0.72)]),
-                        linewidth(2mm),
-                        repcolour) # 4 to 4
-
-  compose(drawnreps[find(x -> x > 0, reps)]...)
+  compose(context(),
+         (context(), line(drawnreps[find(x -> x > 0, reps)]),
+                     linewidth(2mm), repcolour))
 end
 
-function drawacts(acts)
+function drawacts(acts, c1, c2, canvas, rad)
 
-  drawnacts = Array(Context, 16)
+  drawnacts = Array(Any, 16)
   actscolour = fill(LCHab(90, 0, 297))
 
-  drawnacts[1] = compose(context(),
-                         polygon([(0.2, 0.3), (0.16, 0.32), (0.16, 0.28)]),
-                         actscolour) # From 1 to 1.
+  drawnacts[1] = ([(0.2, 0.3), (0.16, 0.32), (0.16, 0.28)]) # From 1 to 1.
+  drawnacts[2] = ([(0.41, 0.27), (0.45, 0.29), (0.45, 0.25)]) # From 2 to 1.
+  drawnacts[3] = ([(0.42, 0.38), (0.43, 0.42), (0.46, 0.39)]) # From 3 to 1.
+  drawnacts[4] = ([(0.33, 0.41), (0.31, 0.45), (0.35, 0.45)]) # From 4 to 1.
+  drawnacts[5] = ([(0.59, 0.33), (0.55, 0.35), (0.55, 0.31)]) # From 1 to 2.
+  drawnacts[6] = ([(0.8, 0.3), (0.84, 0.32), (0.84, 0.28)]) # From 2 to 2.
+  drawnacts[7] = ([(0.73, 0.41), (0.71, 0.45), (0.75, 0.45)]) # From 3 to 2.
+  drawnacts[8] = ([(0.62, 0.42), (0.61, 0.46), (0.58, 0.43)]) # From 4 to 2.
+  drawnacts[9] = ([(0.58, 0.62), (0.57, 0.58), (0.54, 0.61)]) # From 1 to 3.
+  drawnacts[10] = ([(0.67, 0.59), (0.65, 0.55), (0.69, 0.55)]) # From 2 to 3.
+  drawnacts[11] = ([(0.8, 0.7), (0.84, 0.72), (0.84, 0.68)]) # From 3 to 3.
+  drawnacts[12] = ([(0.59, 0.73), (0.55, 0.75), (0.55, 0.71)]) # From 4 to 3.
+  drawnacts[13] = ([(0.27, 0.59), (0.25, 0.55), (0.29, 0.55)]) # From 1 to 4.
+  drawnacts[14] = ([(0.38, 0.58), (0.39, 0.54), (0.42, 0.57)]) # From 2 to 4.
+  drawnacts[15] = ([(0.41, 0.67), (0.45, 0.69), (0.45, 0.65)]) # From 3 to 4.
+  drawnacts[16] = ([(0.2, 0.7), (0.16, 0.72), (0.16, 0.68)]) # From 4 to 4.
 
-  drawnacts[2] = compose(context(),
-                         polygon([(0.41, 0.27), (0.45, 0.29), (0.45, 0.25)]),
-                         actscolour) # From 2 to 1.
-
-  drawnacts[3] = compose(context(),
-                         polygon([(0.42, 0.38), (0.43, 0.42), (0.46, 0.39)]),
-                         actscolour) # From 3 to 1.
-
-  drawnacts[4] = compose(context(),
-                         polygon([(0.33, 0.41), (0.31, 0.45), (0.35, 0.45)]),
-                         actscolour) # From 4 to 1.
-
-  drawnacts[5] = compose(context(),
-                         polygon([(0.59, 0.33), (0.55, 0.35), (0.55, 0.31)]),
-                         actscolour) # From 1 to 2.
-
-  drawnacts[6] = compose(context(),
-                         polygon([(0.8, 0.3), (0.84, 0.32), (0.84, 0.28)]),
-                         actscolour) # From 2 to 2.
-
-  drawnacts[7] = compose(context(),
-                         polygon([(0.73, 0.41), (0.71, 0.45), (0.75, 0.45)]),
-                         actscolour) # From 3 to 2.
-
-  drawnacts[8] = compose(context(),
-                         polygon([(0.62, 0.42), (0.61, 0.46), (0.58, 0.43)]),
-                         actscolour) # From 4 to 2.
-
-  drawnacts[9] = compose(context(),
-                         polygon([(0.58, 0.62), (0.57, 0.58), (0.54, 0.61)]),
-                         actscolour) # From 1 to 3.
-
-  drawnacts[10] = compose(context(),
-                          polygon([(0.67, 0.59), (0.65, 0.55), (0.69, 0.55)]),
-                          actscolour) # From 2 to 3.
-
-  drawnacts[11] = compose(context(),
-                          polygon([(0.8, 0.7), (0.84, 0.72), (0.84, 0.68)]),
-                          actscolour) # From 3 to 3.
-
-  drawnacts[12] = compose(context(),
-                          polygon([(0.59, 0.73), (0.55, 0.75), (0.55, 0.71)]),
-                          actscolour) # From 4 to 3.
-
-  drawnacts[13] = compose(context(),
-                          polygon([(0.27, 0.59), (0.25, 0.55), (0.29, 0.55)]),
-                          actscolour) # From 1 to 4.
-
-  drawnacts[14] = compose(context(),
-                          polygon([(0.38, 0.58), (0.39, 0.54), (0.42, 0.57)]),
-                          actscolour) # From 2 to 4.
-
-  drawnacts[15] = compose(context(),
-                          polygon([(0.41, 0.67), (0.45, 0.69), (0.45, 0.65)]),
-                          actscolour) # From 3 to 4.
-
-  drawnacts[16] = compose(context(),
-                          polygon([(0.2, 0.7), (0.16, 0.72), (0.16, 0.68)]),
-                          actscolour) # From 4 to 4.
-
-  compose(drawnacts[find(x -> x > 0, acts)]...)
+  compose(context(),
+         (context(), polygon(drawnacts[find(x -> x > 0, acts)]), actscolour))
 
 end
 
-function drawpaths(paths)
+function drawpaths(paths, c1, c2, canvas, rad)
 
   drawnpaths = Array(Any, 16)
   pathcolour = stroke(LCHab(90, 0, 297))
@@ -283,7 +181,7 @@ function drawpaths(paths)
 
 end
 
-function drawenvs(envs)
+function drawenvs(envs, c1, c2, canvas, rad)
 
   drawnenvs = Array(Context, 4)
   arrowcol = fill(LCHab(90, 0, 297))
@@ -329,7 +227,7 @@ function drawenvs(envs)
 
 end
 
-function drawgates(gates)
+function drawgates(gates, c1, c2, canvas, rad)
 
   gatehash::Dict{Bool, String} = {1 => "and", 0 => " or"}
 
@@ -350,7 +248,7 @@ function drawgates(gates)
 
 end
 
-function drawgeneric()
+function drawgeneric(canvas, c1, c2, rad)
   # This function draws the generic background that exists for all networks.
   # It includes the background colour, the four nodes (genes), and their
   # labels.
@@ -364,14 +262,12 @@ function drawgeneric()
                      fontsize(38pt),
                      textcolour),
          (context(),
-          circle(0.3, 0.3, 0.1), fill(LCHab(60, 37, 239))),
+          circle([c1, c2, c2, c1], [c1, c1, c2, c2], [rad, rad, rad, rad]),
+          fill([LCHab(60, 37, 239),
+                LCHab(68, 61, 75),
+                LCHab(58, 50, 151),
+                LCHab(51, 72, 19)])),
          (context(),
-          circle(0.7, 0.3, 0.1), fill(LCHab(68, 61, 75))),
-         (context(),
-          circle(0.7, 0.7, 0.1), fill(LCHab(58, 50, 151))),
-         (context(),
-          circle(0.3, 0.7, 0.1), fill(LCHab(51, 72, 19))),
-         (context(),
-          rectangle(0, 0, 1, 1), fill(LCHab(17, 14, 259))))
+          rectangle(0, 0, canvas, canvas), fill(LCHab(17, 14, 259))))
 
 end
