d00556a0ef50ee62c2888862a05333e5ed7a0154
Stephanie Unna <steph.unna@gmail.com>
2015-02-04 15:11:59 +0000

    Beginning conversion to relative (rather than absolute) values.

--- lib/draw4node.jl
+++ lib/draw4node.jl
@@ -17,7 +17,7 @@ function draw4node(reps, acts, envs, gates)
            (context(), drawpaths(acts + reps, c1, c2, canvas, rad)),
            (context(), drawenvs(envs, c1, c2, canvas, rad)),
            (context(), drawgates(gates, c1, c2, canvas, rad)),
-           (context(), drawgeneric(canvas, c1, c2, rad, rad)))
+           (context(), drawgeneric(canvas, c1, c2, rad)))
   end
 
   img = PNG("../runs/netpic.png", 8inch, 8inch)
@@ -31,22 +31,27 @@ drawnreps = Array(Any, 16)
 
 repcolour = stroke(LCHab(90, 0, 297))
 
-drawnreps[1] = ([(0.19, 0.28), (0.19, 0.32)]) # 1 to 1
-drawnreps[2] = ([(0.42, 0.25), (0.42, 0.29)]) # 2 to 1
+d1 = rad/10
+d2 = d1*2
+p1 = c1 - (d1 + d2)
+p2 = c1 + (d1 + d2)
+
+drawnreps[1] = ([(c1 - (rad + d1), c1 - d2), (c1 - (rad + d1), c1 + d2)]) # 1 to 1
+drawnreps[2] = ([(c1 + rad + d2, 0.25), (c1 + rad + d2, 0.29)]) # 2 to 1
 drawnreps[3] = ([(0.415, 0.405), (0.445, 0.375)]) # 3 to 1
-drawnreps[4] = ([(0.31, 0.42), (0.35, 0.42)]) # 4 to 1
-drawnreps[5] = ([(0.58, 0.31), (0.58, 0.35)]) # 1 to 2
-drawnreps[6] = ([(0.81, 0.28), (0.81, 0.32)]) # 2 to 2
-drawnreps[7] = ([(0.71, 0.42), (0.75, 0.42)]) # 3 to 2
+drawnreps[4] = ([(0.31, c1 + rad + d2), (0.35, c1 + rad + d2)]) # 4 to 1
+drawnreps[5] = ([(c2 - (rad + d2), 0.31), (c2 - (rad + d2), 0.35)]) # 1 to 2
+drawnreps[6] = ([(c2 + rad + d1, c1 - d2), (c2 + rad + d1, c1 + d2)]) # 2 to 2
+drawnreps[7] = ([(0.71, c1 + rad + d2), (0.75, c1 + rad + d2)]) # 3 to 2
 drawnreps[8] = ([(0.595, 0.415), (0.625, 0.445)]) # 4 to 2
 drawnreps[9] = ([(0.555, 0.625), (0.585, 0.595)]) # 1 to 3
-drawnreps[10] = ([(0.65, 0.58), (0.69, 0.58)]) # 2 to 3
-drawnreps[11] = ([(0.81, 0.68), (0.81, 0.72)]) # 3 to 3
-drawnreps[12] = ([(0.58, 0.71), (0.58, 0.75)]) # 4 to 3
-drawnreps[13] = ([(0.25, 0.58), (0.29, 0.58)]) # 1 to 4
+drawnreps[10] = ([(0.65, c2 - (rad + d2)), (0.69, c2 - (rad + d2))]) # 2 to 3
+drawnreps[11] = ([(c2 + rad + d1, 0.68), (c2 + rad + d1, 0.72)]) # 3 to 3
+drawnreps[12] = ([(c2 - (rad + d2), 0.71), (c2 - (rad + d2), 0.75)]) # 4 to 3
+drawnreps[13] = ([(0.25, c2 - (rad + d2)), (0.29, c2 - (rad + d2))]) # 1 to 4
 drawnreps[14] = ([(0.375, 0.555), (0.405, 0.585)]) # 2 to 4
-drawnreps[15] = ([(0.42, 0.65), (0.42, 0.69)]) # 3 to 4
-drawnreps[16] = ([(0.19, 0.68), (0.19, 0.72)]) # 4 to 4
+drawnreps[15] = ([(c1 + rad + d2, 0.65), (c1 + rad + d2, 0.69)]) # 3 to 4
+drawnreps[16] = ([(c1 - (rad + d1), 0.68), (c1 - (rad + d1), 0.72)]) # 4 to 4
 
   compose(context(),
          (context(), line(drawnreps[find(x -> x > 0, reps)]),
@@ -58,20 +63,25 @@ function drawacts(acts, c1, c2, canvas, rad)
   drawnacts = Array(Any, 16)
   actscolour = fill(LCHab(90, 0, 297))
 
-  drawnacts[1] = ([(0.2, 0.3), (0.16, 0.32), (0.16, 0.28)]) # From 1 to 1.
+  d1 = rad/10
+  d2 = d1*2
+  p1 = c1 - (d1 + d2)
+  p2 = c1 + (d1 + d2)
+
+  drawnacts[1] = ([(0.2, 0.3), (0.16, c1 + d2), (0.16, c1 - d2)]) # From 1 to 1.
   drawnacts[2] = ([(0.41, 0.27), (0.45, 0.29), (0.45, 0.25)]) # From 2 to 1.
-  drawnacts[3] = ([(0.42, 0.38), (0.43, 0.42), (0.46, 0.39)]) # From 3 to 1.
+  drawnacts[3] = ([(c1 + rad + d2, 0.38), (0.43, c1 + rad + d2), (0.46, 0.39)]) # From 3 to 1.
   drawnacts[4] = ([(0.33, 0.41), (0.31, 0.45), (0.35, 0.45)]) # From 4 to 1.
   drawnacts[5] = ([(0.59, 0.33), (0.55, 0.35), (0.55, 0.31)]) # From 1 to 2.
-  drawnacts[6] = ([(0.8, 0.3), (0.84, 0.32), (0.84, 0.28)]) # From 2 to 2.
+  drawnacts[6] = ([(0.8, 0.3), (0.84, c1 + d2), (0.84, c1 - d2)]) # From 2 to 2.
   drawnacts[7] = ([(0.73, 0.41), (0.71, 0.45), (0.75, 0.45)]) # From 3 to 2.
-  drawnacts[8] = ([(0.62, 0.42), (0.61, 0.46), (0.58, 0.43)]) # From 4 to 2.
+  drawnacts[8] = ([(0.62, c1 + rad + d2), (0.61, 0.46), (0.58, 0.43)]) # From 4 to 2.
   drawnacts[9] = ([(0.58, 0.62), (0.57, 0.58), (0.54, 0.61)]) # From 1 to 3.
   drawnacts[10] = ([(0.67, 0.59), (0.65, 0.55), (0.69, 0.55)]) # From 2 to 3.
   drawnacts[11] = ([(0.8, 0.7), (0.84, 0.72), (0.84, 0.68)]) # From 3 to 3.
   drawnacts[12] = ([(0.59, 0.73), (0.55, 0.75), (0.55, 0.71)]) # From 4 to 3.
   drawnacts[13] = ([(0.27, 0.59), (0.25, 0.55), (0.29, 0.55)]) # From 1 to 4.
-  drawnacts[14] = ([(0.38, 0.58), (0.39, 0.54), (0.42, 0.57)]) # From 2 to 4.
+  drawnacts[14] = ([(0.38, 0.58), (0.39, 0.54), (c1 + rad + d2, 0.57)]) # From 2 to 4.
   drawnacts[15] = ([(0.41, 0.67), (0.45, 0.69), (0.45, 0.65)]) # From 3 to 4.
   drawnacts[16] = ([(0.2, 0.7), (0.16, 0.72), (0.16, 0.68)]) # From 4 to 4.
 
@@ -85,16 +95,21 @@ function drawpaths(paths, c1, c2, canvas, rad)
   drawnpaths = Array(Any, 16)
   pathcolour = stroke(LCHab(90, 0, 297))
 
+  d1 = rad/10
+  d2 = d1*2
+  p1 = c1 - (d1 + d2)
+  p2 = c1 + (d1 + d2)
+
   drawnpaths[1] = (context(),
-                          curve([(0.19, 0.30), (0.15, 0.15)],
-                                [(0.12, 0.30), (0.21, 0.09)],
-                                [(0.09, 0.21), (0.30, 0.12)],
-                                [(0.15, 0.15), (0.30, 0.19)]),
+                          curve([(0.19, c1), (0.15, 0.15)],
+                                [(0.12, c1), (0.21, 0.09)],
+                                [(0.09, 0.21), (c1, 0.12)],
+                                [(0.15, 0.15), (c1, 0.19)]),
                           fill(nothing), linewidth(1mm),
                           pathcolour) # From 1 to 1.
 
   drawnpaths[2] = (context(),
-                          line([(0.42, 0.27), (0.58, 0.27)]),
+                          line([(c1 + rad + d2, 0.27), (0.58, 0.27)]),
                           linewidth(1mm),
                           pathcolour) # From 2 to 1.
 
@@ -104,25 +119,25 @@ function drawpaths(paths, c1, c2, canvas, rad)
                           pathcolour) # From 3 to 1.
 
   drawnpaths[4] = (context(),
-                          line([(0.33, 0.42), (0.33, 0.58)]),
+                          line([(0.33, c1 + rad + d2), (0.33, 0.58)]),
                           linewidth(1mm),
                           pathcolour) # From 4 to 1.
 
   drawnpaths[5] = (context(),
-                          line([(0.42, 0.33), (0.58, 0.33)]),
+                          line([(c1 + rad + d2, 0.33), (0.58, 0.33)]),
                           linewidth(1mm),
                           pathcolour) # From 1 to 2.
 
   drawnpaths[6] = (context(),
-                          curve([(0.81, 0.3), (0.85, 0.15)],
-                                [(0.88, 0.3), (0.79, 0.09)],
-                                [(0.91, 0.21), (0.7, 0.12)],
-                                [(0.85, 0.15), (0.7, 0.19)]),
+                          curve([(0.81, c1), (0.85, 0.15)],
+                                [(0.88, c1), (0.79, 0.09)],
+                                [(0.91, 0.21), (c2, 0.12)],
+                                [(0.85, 0.15), (c2, 0.19)]),
                           fill(nothing), linewidth(1mm),
                           pathcolour) # From 2 to 2.
 
   drawnpaths[7] = (context(),
-                          line([(0.73, 0.42), (0.73, 0.58)]),
+                          line([(0.73, c1 + rad + d2), (0.73, 0.58)]),
                           linewidth(1mm),
                           pathcolour) # From 3 to 2.
 
@@ -137,25 +152,25 @@ function drawpaths(paths, c1, c2, canvas, rad)
                           pathcolour) # From 1 to 3.
 
   drawnpaths[10] = (context(),
-                           line([(0.67, 0.58), (0.67, 0.42)]),
+                           line([(0.67, 0.58), (0.67, c1 + rad + d2)]),
                            linewidth(1mm),
                            pathcolour) # From 2 to 3.
 
   drawnpaths[11] = (context(),
-                           curve([(0.19, 0.7), (0.15, 0.85)],
-                                 [(0.12, 0.7), (0.21, 0.91)],
-                                 [(0.09, 0.79), (0.3, 0.88)],
-                                 [(0.15, 0.85), (0.3, 0.81)]),
+                           curve([(0.19, c2), (0.15, 0.85)],
+                                 [(0.12, c2), (0.21, 0.91)],
+                                 [(0.09, 0.79), (c1, 0.88)],
+                                 [(0.15, 0.85), (c1, 0.81)]),
                            fill(nothing), linewidth(1mm),
                            pathcolour) # From 3 to 3.
 
   drawnpaths[12] = (context(),
-                           line([(0.42, 0.73), (0.58, 0.73)]),
+                           line([(c1 + rad + d2, 0.73), (0.58, 0.73)]),
                            linewidth(1mm),
                            pathcolour) # From 4 to 3.
 
   drawnpaths[13] = (context(),
-                           line([(0.27, 0.58), (0.27, 0.42)]),
+                           line([(0.27, 0.58), (0.27, c1 + rad + d2)]),
                            linewidth(1mm),
                            pathcolour) # From 1 to 4.
 
@@ -165,15 +180,15 @@ function drawpaths(paths, c1, c2, canvas, rad)
                            pathcolour) # From 2 to 4.
 
   drawnpaths[15] = (context(),
-                           line([(0.42, 0.67), (0.58, 0.67)]),
+                           line([(c1 + rad + d2, 0.67), (0.58, 0.67)]),
                            linewidth(1mm),
                            pathcolour) # From 3 to 4.
 
   drawnpaths[16] = (context(),
-                           curve([(0.81, 0.7), (0.85, 0.85)],
-                                 [(0.88, 0.7), (0.79, 0.91)],
-                                 [(0.91, 0.79), (0.7, 0.88)],
-                                 [(0.85, 0.85), (0.7, 0.81)]),
+                           curve([(0.81, c2), (0.85, 0.85)],
+                                 [(0.88, c2), (0.79, 0.91)],
+                                 [(0.91, 0.79), (c2, 0.88)],
+                                 [(0.85, 0.85), (c2, 0.81)]),
                            fill(nothing), linewidth(1mm),
                            pathcolour) # From 4 to 4.
 
