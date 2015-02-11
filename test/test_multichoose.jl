608a1a9638219367c1ff44f906de3e394288fb3c
Stephanie Unna <steph.unna@gmail.com>
2015-02-11 15:22:41 +0000

    Making some notes for improvements later on (when I can get FactCheck to install.

--- test/test_multichoose.jl
+++ test/test_multichoose.jl
@@ -6,7 +6,7 @@ include("./multichoose.jl")
 facts("Checking simple examples work") do
   #TODO: The matrix constructed here does not actually need to be identical
   #      to the one made in multichoose, they just need to contain the same
-  #      rows (in any order).
+  #      rows (in any order). Could use something like all(in(x[i], y))
   expression1 = multichoose(Array[[0, 1], [0, 1]], 0)
   expression2 = multichoose(Array[[0, 1], [0, 1]], 1)
   assertion1 = [0 0; 1 0; 0 1; 1 1]
@@ -14,4 +14,4 @@ facts("Checking simple examples work") do
   @fact expression1 => assertion1
 end
 
-end
+end # TestMultichoose module
