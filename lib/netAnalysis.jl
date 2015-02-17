9cffb3e467a793c926901d17f18ab9cb28889fc0
Stephanie Unna <steph.unna@gmail.com>
2015-02-17 12:50:03 +0000

    Removing prune cycles since no longer necessary.

--- lib/netAnalysis.jl
+++ lib/netAnalysis.jl
@@ -30,24 +30,24 @@ function count_cycles(net::Network, params::Dict)
 
   cycles = Array{Int64}[]
 
-  routematrix = zeros(Int64, params["nnodes"], params["nnodes"])
+  traversed = zeros(Int64, params["nnodes"], params["nnodes"])
   routearray = Int64[]
 
   for n in 1:params["nnodes"]     # Looping over each node.
 
-    # Setting the routematrix so that paths cannot go to genes lower than
+    # Setting the traversed so that paths cannot go to genes lower than
     # node that we are finding cycles from. This should only happen if we
     # are on the top level (i.e. not in a recursive call) since it is due
     # to double-counting of cycles from different nodes (e.g. 1-2-2-3-3-1
     # and 2-3-3-1-1-2).
     for j in 1:(n - 1)
-      routematrix[:, j] = 1
-      routematrix[j, :] = 1
+      traversed[:, j] = 1
+      traversed[j, :] = 1
     end
 
-    println("routematrix before find_cycles\n$routematrix")
+    println("traversed before find_cycles\n$traversed")
 
-    explorer = @task find_cycles_from(n, routematrix, routearray,
+    explorer = @task find_cycles_from(n, traversed, routearray,
                                       params["nnodes"], activepaths)
 
     while !istaskdone(explorer)
@@ -56,17 +56,15 @@ function count_cycles(net::Network, params::Dict)
     end
 
   end
-  println("cycles before pruning:\n$cycles")
-  # cycles = prunecycles!(cycles)
-  # println("cycles after pruning:\n$cycles")
+  println("cycles:\n$cycles")
   return length(cycles)
 end
 
-# routematrix will be nnodes x nnodes matrix with ones entered into paths
+# traversed will be nnodes x nnodes matrix with ones entered into paths
 # that have been traversed already.
 # routearray will be an array with the list of nodes in the current route
 
-function find_cycles_from(node::Int64, routematrix, routearray,
+function find_cycles_from(node::Int64, traversed, routearray,
                           nnodes, activepaths)
 
   for m in 1:nnodes # Looping over all outgoing paths.
@@ -75,20 +73,22 @@ function find_cycles_from(node::Int64, routematrix, routearray,
     # and if either routearray is empty, or the first element is
     # not equal to the last (i.e. not already a cycle):
     if (activepaths[node, m] != (0, 0)) &&
-       (routematrix[node, m] == 0) &&
+       (traversed[node, m] == 0) &&
        (routearray == [] ||
        routearray[1] != routearray[end])
       thisroutearr = copy(routearray)
       push!(thisroutearr, activepaths[node, m]...)
-      thisroutemat = copy(routematrix)
-      thisroutemat[node, m] = 1
-      println("route matrix:\n$thisroutemat")
-      find_cycles_from(m, thisroutemat, thisroutearr, nnodes, activepaths)
+      thistraversed = copy(traversed)
+      thistraversed[node, m] = 1
+      println("traversed matrix:\n$thistraversed")
+      find_cycles_from(m, thistraversed, thisroutearr, nnodes, activepaths)
     end
     if length(routearray) > 0 && activepaths[node, m][2] == routearray[1]
       if routearray[1] == routearray[end]
+        println("case 1")
         produce(routearray)
       else
+        println("case 2")
         produce([routearray, activepaths[node, m]...])
       end
     end
@@ -124,36 +124,5 @@ function isless(lhs::Array, rhs::Array)
   length(lhs) < length(rhs)
 end
 
-# function prunecycles!(cycles::Array{Array{Int64}})
-#
-#   sort!(cycles, ; rev = true)
-#   # Since cycles are now in ascending order, only need to check
-#   # each one with the following ones to see if it is a subset.
-#   for i in 1:length(cycles) - 1
-#     for j in (i + 1):length(cycles)
-#       for k in 1:length(cycles[i])
-#         if (k == 1) && (find(x -> x == cycles[i][k]) != [])
-#         elseif (k > 1) && in(cycles[j][k], find(x -> x == cycles[i][k]) != [])
-#         else
-#           break
-#
-#
-#
-#
-#   for i in 1:length(cycles)
-#     for j in 1:length(cycles)
-#       if findin(cycles[i], cycles[j])
-#       indices = findin(cycles[i], cycles[j])
-#       # See whether the indices are consecutive numbers:
-#       if all(t -> indices[t] == indices[t + 1] - 1, 1:(length(indices) - 1))
-#         cycles[findfirst(r -> r == cycles[j], cycles)] = [0]
-#       end
-#     end
-#   end
-#   println("cycles before splice: $cycles")
-#   deleteat!(cycles, find(r -> r == [0], cycles))
-#   return cycles
-#
-# end
 
 end # netAnalysis module
