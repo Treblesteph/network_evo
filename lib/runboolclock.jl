48882530844d14de0993261b4072fdb2b61e8005
Stephanie Unna <steph.unna@gmail.com>
2015-02-19 15:22:27 +0000

    Putting all clock-related parameters into their own file (and module) for ease of transitioning between different environmental conditions.

--- lib/runboolclock.jl
+++ lib/runboolclock.jl
@@ -11,35 +11,11 @@ import BoolNetwork.make_decision_mat,
        BoolNetwork.noInteraction,
        BoolNetwork.net2hash
 
-function add_clock_params!(params::Dict, interactions::Array)
-  interactiontypes = [repression, activation, noInteraction]
-  params["interacttypes"] = interactiontypes
-
-  dawnwindow = 3 * 60
-  duskwindow = 3 * 60
-  daytime = 12 * 60
-
-  days = zeros(Int64, params["alldays"], daytime)
-  dawns = zeros(Int64, params["alldays"], dawnwindow)
-  dusks = zeros(Int64, params["alldays"], duskwindow)
-
-  for t = 1:params["alldays"] # Converting to arrays of minutes.
-    days[t, :] = (1 + 60 * 24 * (t - 1)):(daytime + 24 * 60 * (t - 1))
-    dawns[t, :] = (1 + 60 * 24 * (t - 1)):(dawnwindow + 24 * 60 * (t - 1))
-    dusks[t, :] = (1 + (12 * 60 - duskwindow +
-                   24 * 60 * (t - 1))):(60 * (12 + 24 * (t - 1)))
-  end
-
-  params["envsignal"] = days
-  params["gene1fit"] = dawns
-  params["gene2fit"] = dusks
-end
-
 function runclock()
 
   tic()
   params = set_parameters()
-  add_clock_params!(params, [repression, activation, noInteraction])
+  add_clock_params!(params, single_pp!)
   model = runga(params, EvolveClock; init_pop_size = 50, stop_after = 25000)
   println()
   toc()
