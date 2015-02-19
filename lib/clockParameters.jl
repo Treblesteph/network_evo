0b339972a362a44fb0f1c75656d2bcd3defd5425
Stephanie Unna <steph.unna@gmail.com>
2015-02-19 15:31:26 +0000

    Turning ClockParameters into a module, and modifying input/output of various functions for different environmental conditions.

--- lib/clockParameters.jl
+++ lib/clockParameters.jl
@@ -1,10 +1,30 @@
+module ClockParameters
+
+export add_clock_params!,
+       single_pp!,
+       multi_pp!,
+       single_pp_noise!,
+       multi_pp_noise!,
+       harvard_forest
 
 
 function add_clock_params!(params::Dict, envconditions::Function)
-  dawnwindow = 3 * 60
-  duskwindow = 3 * 60
 
   params = envconditions(params)
+
+  params["allhours"] = params["alldays"] * 24
+  params["allmins"] = params["allhours"] * 60
+
+  dawnwindow = 3 * 60
+  duskwindow = 3 * 60
+  dawns = zeros(Int64, params["alldays"], dawnwindow)
+  dusks = zeros(Int64, params["alldays"], duskwindow)
+
+  for t = 1:params["alldays"] # Converting to arrays of minutes.
+    dawns[t, :] = (1 + 60 * 24 * (t - 1)):(dawnwindow + 24 * 60 * (t - 1))
+    dusks[t, :] = (1 + (12 * 60 - duskwindow +
+    24 * 60 * (t - 1))):(60 * (12 + 24 * (t - 1)))
+  end
 
   params["interacttypes"] = [repression, activation, noInteraction]
   params["envsignal"] = days
@@ -12,7 +32,7 @@ function add_clock_params!(params::Dict, envconditions::Function)
   params["gene2fit"] = dusks
 end
 
-function single_pp(params::Dict)
+function single_pp!(params::Dict)
 
   lightperiod = 12
   alldays = Int64[4]
@@ -25,12 +45,11 @@ function single_pp(params::Dict)
   end
 
   params["alldays"] = alldays[1]
-  params["allhours"] = alldays[1] * 24
-  params["allmins"] = alldays[1] * 24 * 60
+
   return params
 end
 
-function multi_pp(params::Dict)
+function multi_pp!(params::Dict)
 
   nphotoperiod = 9
   minlightperiod = 6
@@ -41,31 +60,29 @@ function multi_pp(params::Dict)
   days = [zeros(Int64, alldays/nphotoperiod, daytime[k]) for k in 1:nphotoperiod]
 
   params["alldays"] = alldays[1]
-  params["allhours"] = alldays[1] * 24
-  params["allmins"] = alldays[1] * 24 * 60
+
   return params
 end
 
-function single_pp_noise(params::Dict)
+function single_pp_noise!(params::Dict)
 
   params["alldays"] = alldays[1]
-  params["allhours"] = alldays[1] * 24
-  params["allmins"] = alldays[1] * 24 * 60
+
   return params
 end
 
-function multi_pp_noise(params::Dict)
+function multi_pp_noise!(params::Dict)
 
   params["alldays"] = alldays[1]
-  params["allhours"] = alldays[1] * 24
-  params["allmins"] = alldays[1] * 24 * 60
+
   return params
 end
 
-function harvard_forest(params::Dict)
+function harvard_forest!(params::Dict)
 
   params["alldays"] = alldays[1]
-  params["allhours"] = alldays[1] * 24
-  params["allmins"] = alldays[1] * 24 * 60
+
   return params
 end
+
+end # ClockParameters module
