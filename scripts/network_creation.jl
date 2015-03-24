49b36a23a7d761c19ac1ec36aeb989f24435531f
Stephanie Unna <steph.unna@gmail.com>
2015-03-24 16:52:14 +0000

    Making some more test networks to check the output of longer runs.

--- scripts/network_creation.jl
+++ scripts/network_creation.jl
@@ -4,23 +4,23 @@ import BoolNetwork
 include("../lib/runboolclock.jl")
 
 params = set_parameters()
-add_clock_params!(params, 9, false)
+add_clock_params!(params, 9, true)
 
 x = params["minlag"]
 
-acts = [round(0*60), round(0*60), round(9.98*60), round(0*60),
+acts = [round(0*60), round(0*60), round(12.95*60), round(0*60),
         round(0*60), round(0*60), round(0*60), round(0*60),
         round(0*60), round(0*60), round(0*60), round(0*60),
         round(0*60), round(0*60), round(0*60), round(0*60)]
 
 reps = [round(0*60), round(0*60), round(0*60), round(0*60),
-        round(0*60), round(0*60), round(6.98*60), round(0*60),
-        round(0*60), round(0*60), round(0*60), round(0*60),
+        round(0*60), round(0*60), round(0*60), round(11.13*60),
+        round(0*60), round(0*60), round(3.2*60), round(6.9*60),
         round(0*60), round(0*60), round(0*60), round(0*60)]
 
-envs = [round(0*60), round(0.08*60), round(13.98*60), round(0*60)]
+envs = [round(0*60), round(0.08*60), round(10.8*60), round(8.52*60)]
 
-gates = [true, false, true, true]
+gates = [false, true, false, false]
 
 net = Network(convert(Array{Int64}, acts), convert(Array{Int64}, reps),
               gates, convert(Array{Int64}, envs), params)
